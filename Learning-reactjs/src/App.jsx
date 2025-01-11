aafca108616d2c99af882d170ddf967e85d0ac6e
MatamaPeter <peter24matama@gmail.com>
2025-01-11 11:53:14 +0300

    starting files

---
+++ Learning-reactjs/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Classbasedcomponent from './components/class-based-components'
+
+function App() {
+
+  return (
+    <div>
+      <h1>My First React App</h1>
+      <Classbasedcomponent/>
+    </div>
+
+  )
+}
+
+export default App
---
+++ Learning-reactjs/src/components/class-based-components.jsx
@@ -0,0 +1,15 @@
+import { Component } from "react"
+
+
+
+class Classbasedcomponent extends Component {
+    render() {
+        return (
+            <div>
+                <h3>This is a class based component</h3>
+            </div>
+        )
+    }
+}
+
+export default Classbasedcomponent;
